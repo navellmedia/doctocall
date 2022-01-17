0fe283c9a66c943456328e512be3a4a199a93202
ardiesta.code <ardiesta.code@gmail.com>
2022-01-17 21:10:51 +0700

    add Firebase database

--- lib/features/firebase_reature/models/kategori_model.dart
+++ lib/features/firebase_reature/models/kategori_model.dart
@@ -10,19 +10,23 @@ String kategoryToJson(Kategory data) => json.encode(data.toJson());
 
 class Kategory {
   Kategory({
+    this.id,
     this.kode,
     this.kategori,
   });
 
+  String? id;
   String? kode;
   String? kategori;
 
   factory Kategory.fromJson(Map<String, dynamic> json) => Kategory(
+    id: json["id"],
     kode: json["kode"],
     kategori: json["kategori"],
   );
 
   Map<String, dynamic> toJson() => {
+    "id": id,
     "kode": kode,
     "kategori": kategori,
   };
--- lib/features/firebase_reature/screens/firebase_screen.dart
+++ lib/features/firebase_reature/screens/firebase_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../models/kategori_model.dart';
 
@@ -14,15 +15,21 @@ class _FirebaseScreenState extends State<FirebaseScreen> {
   CollectionReference kategory =
       FirebaseFirestore.instance.collection('kategory');
 
+  List<Kategory> listKategory = [];
+
   //CRUD (Create, Read, Update, Delete)
   Future<void> readDataKategory() async {
-    QuerySnapshot result = await kategory.get();
+    QuerySnapshot result = await kategory //.where("kode", whereIn: ['SPT06'])
+        .get();
     List<Kategory> data = result.docs.map((e) {
-      return Kategory.fromJson(e.data() as Map<String, dynamic>);
+      var mapData = e.data() as Map<String, dynamic>;
+      mapData.addAll({"id": e.id});
+
+      return Kategory.fromJson(mapData);
     }).toList();
 
-    data.forEach((element) {
-      print("${element.kode}-${element.kategori}");
+    setState(() {
+      listKategory = data;
     });
   }
 
@@ -36,24 +43,44 @@ class _FirebaseScreenState extends State<FirebaseScreen> {
         .catchError((error) => print("insert error $error"));
   }
 
+  Future<void> deleteData(String? id) async {
+    print(id!);
+    await kategory.doc(id).delete().then((value) {
+      print("delete sukses");
+      readDataKategory();
+    }).catchError((error) => print("delete error $error"));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Fibase"),
-      ),
-      body: ListView(
-        children: [
-          RaisedButton(
-            onPressed: readDataKategory,
-            child: Text("Get Data"),
-          ),
-          RaisedButton(
-            onPressed: addData,
-            child: Text("Add Data"),
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Fibase"),
+          actions: [
+            IconButton(
+                onPressed: readDataKategory,
+                icon: FaIcon(FontAwesomeIcons.search)),
+            IconButton(onPressed: addData, icon: FaIcon(FontAwesomeIcons.save)),
+          ],
+        ),
+        body: ListView.builder(
+            itemCount: listKategory.length,
+            itemBuilder: (BuildContext ctx, int idx) {
+              Kategory item = listKategory[idx];
+
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.orange,
+                  child: Text(
+                    "A",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                title: Text(item.kategori!),
+                onLongPress: () async {
+                  await deleteData(item.id!);
+                },
+              );
+            }));
   }
 }
